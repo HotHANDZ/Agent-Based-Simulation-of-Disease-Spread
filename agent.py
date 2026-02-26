2fd45839654967e2c5c2d15c1ab2f05ed5592e41
HotHANDZ <Bwayo115@gmail.com>
2026-02-26 17:19:01 -0500

    Added more code to the Agent file.  Seems to be in a good place for starters

--- agent.py
+++ agent.py
@@ -18,5 +18,34 @@ class Agent:
         self.state = state  # Susceptible, Infected or Recovered
         self.time_infected = 0 # Timeframe used to determine if current agent has had ample time to recover from infection (MAY CHANGE)
 
+    #Move rules defined for the agent with tweakable stepping size
+    def move(self, width, height, step_size=1):
+            
+            #Set a range between -1 and 1 that the agent can move.  Makes for unpredictable movement
+            dx = random.uniform(-step_size, step_size)
+            dy = random.uniform(-step_size, step_size)
+
+            #Add the previously attained value for the random movement of the x and y coordinates and add them to existing x and y coordinate
+            self.x = max(0, min(width, self.x + dx))
+            self.y = max(0, min(height, self.y + dy))
+
+        
+    #Using the Euclidean Distance Formula to calculate the current distance from current agent to another agent
+    def distance(self, other_agent):
+        return math.sqrt((self.x - other_agent.x) ** 2 + (self.y - other_agent.y) ** 2)
 
     
+    #Simple implementation of getting "infected" by checking the current state of the agent and verifying it is "Susceptible" before infecting
+    def infect(self):
+        
+        
+        #Sets the state to infected if current state is susceptible and sets the time to 0
+        if self.state == "Susceptible":
+            self.state = "Infected"
+            self.time_infected = 0 #Value will increment up as time goes on until specified value for recovery
+
+
+
+    #Once the time hits the necessary recovered limit, change the value from "Infected" to "Recovered"
+    def recover(self):
+        self.state = "Recovered"
